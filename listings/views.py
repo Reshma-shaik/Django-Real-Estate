c90bcb6a8ee75d088316a573cda71a8c723761e0
R <reshmarmk33@gmail.com>
2022-01-19 16:36:27 +0530

    Listings and Single Listing

--- listings/views.py
+++ listings/views.py
@@ -1,12 +1,24 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.shortcuts import get_object_or_404, render
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from .models import Listings
 # Create your views here.
 def index(request):
-    return render(request, 'listings/listings.html')
+    listings = Listings.objects.order_by('-list_date')
+    paginator = Paginator(listings, 6) # Show 6 listings per page.
 
-def listing(request):
-    return render(request, 'listings/listing.html')
+    page = request.GET.get('page')
+    paged_listings = paginator.get_page(page)
+    context = {
+        "listings": paged_listings
+    }
+    return render(request, 'listings/listings.html', context)
+
+def listing(request, listing_id):
+    listing = get_object_or_404(Listings, pk=listing_id)
+    context = {
+        "listing": listing
+    }
+    return render(request, 'listings/listing.html', context)
 
 def search(request):
     return render(request, 'listings/search.html')
--- pages/views.py
+++ pages/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
+from listings.models import Listings
+from realtors.models import Realtor
 # Create your views here.
 def index(request):
-    return render(request, 'pages/index.html')
+    listings = Listings.objects.order_by('-list_date')[:3]
+    context = {
+        'listings': listings
+    }
+    return render(request, 'pages/index.html', context)
 
 def about(request):
-    return render(request, 'pages/about.html')
+    realtors = Realtor.objects.order_by('-hire_date')
+    context = {
+        'realtors': realtors
+    }
+    return render(request, 'pages/about.html', context)
